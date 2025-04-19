ac438fe8b3d02d4d184d9d0c5a0d17e35a2207bf
Kark Angelo <karkangelovaronapada@gmail.com>
2025-04-19 19:32:18 +0800

    Ready for alpha release

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SimpliPy_ML",  # Replace with your library's name
+    version="0.1.0",  # Replace with your library's version
+    description="A Python library for machine learning utilities and tools",
+    author="Your Name",
+    author_email="karkangelovaronapada@gmail.com",
+    url="https://github.com/KarkAngelo114/SimpliPy_ML",  # Replace with your GitHub repo URL
+    packages=find_packages(),  # Automatically find and include all packages
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",  # Specify the minimum Python version
+)
